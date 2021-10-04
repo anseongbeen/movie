5011a6960d750eca6c0949257c615643ba4272ae
anseongbeen <ajh2894@gmail.com>
2021-10-04 16:14:48 +0900

    무비

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import Auth from './hoc/auth';
+import MovieDetail from './components/views/MovieDetail/MovieDetail';
+
 
 function App() {
   return (
@@ -28,7 +30,9 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)}/>
           <Route exact path="/login" component={Auth(LoginPage, false)}/>
           <Route exact path="/register" component={Auth(RegisterPage, false)}/>
-
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
+        
+        
         </Switch>
       </div>
     </Router>
---
+++ client/src/components/views/Config.js
@@ -0,0 +1,5 @@
+export const USER_SERVER = 'api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'dd38bfc3f1f258c9f5a22ebd85006e1a';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
